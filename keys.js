fd72062b5855c03734ebf2cb0ed29906ba5cf3aa
Kaushik Devulapalli <kaushik145@gmail.com>
2017-07-24 19:20:32 -0400

    liri bot

---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitterKeys = {
+  consumer_key: '1vFHMw6oCmQxQSjtk3kGWCtbj',
+  consumer_secret: 'L5wLPvAZZzdOOXAqT3BkptbdxRIR5csuAONFQkDHQ1TPo1I7oO',
+  access_token_key: '94667996-L6yZ1tJfD87ncmkn69KbvNcJN4iYoSHyRiKHgjUIi',
+  access_token_secret: 'wne9nROd6RkSlyOTjBk0MxPFviKMWxNUvekQVRcl2AQUB',
+}
+
+exports.spotifyKeys = {
+	id: 'f180ed87ce664683b325f1f75dd4b467',
+	secret: '4a3f519449444e4490bb155f218bdbf8'
+}
